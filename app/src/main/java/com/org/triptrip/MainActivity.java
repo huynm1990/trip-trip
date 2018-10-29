9aa9713734232920dbadf9a5cc4ba7a04fe44b49
Huy Nguyen <nguyenminhhuy08ct@gmail.com>
2018-10-29 13:15:17 +0700

    Change Home to use fragment

--- app/src/main/java/com/org/triptrip/MainActivity.java
+++ app/src/main/java/com/org/triptrip/MainActivity.java
@@ -12,50 +12,11 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    GridView androidGridView;
-
-    String[] gridViewString = {
-            "Events", "Experience", "Services", "Questions and Answers"
-    } ;
-
-
-    int[] gridViewImageId = {
-            R.drawable.travel_guideline,
-            R.drawable.travel_experience,
-            R.drawable.travel_services,
-            R.drawable.travel_discussion
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*CustomGridViewActivity adapterViewAndroid = new CustomGridViewActivity(MainActivity.this, gridViewString, gridViewImageId);
-        androidGridView=(GridView)findViewById(R.id.grid_view_image_text);
-        androidGridView.setAdapter(adapterViewAndroid);
-        androidGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int i, long id) {
-            if (i == 0) {
-                Intent intent = new Intent(MainActivity.this, EventsActivity.class);
-                startActivity(intent);
-            } else if (i == 1) {
-                Intent intent = new Intent(MainActivity.this, ExperiencesActivity.class);
-                startActivity(intent);
-            } else if (i == 2) {
-                Intent intent = new Intent(MainActivity.this, ServicesActivity.class);
-                startActivity(intent);
-            } else if (i == 3) {
-                Intent intent = new Intent(MainActivity.this, QuestionsAnswersActivity.class);
-                startActivity(intent);
-            }
-
-                *//*Toast.makeText(MainActivity.this, "GridView Item: " + gridViewString[+i], Toast.LENGTH_LONG).show();*//*
-            }
-        });*/
     }
 
     @Override
