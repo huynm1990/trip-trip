253fb7b6b3eb652c42a06c6202091b59251ae606
Huy Nguyen <nguyenminhhuy08ct@gmail.com>
2018-10-29 14:04:15 +0700

    Add Category

--- app/src/main/java/com/org/triptrip/CategoriesAdapter.java
+++ app/src/main/java/com/org/triptrip/CategoriesAdapter.java
@@ -1,9 +1,11 @@
 package com.org.triptrip;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,6 +14,11 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.Vi
 
     private String[] captions;
     private int[] imageIds;
+    private Listener listener;
+
+    public static interface Listener {
+        public void onClick(int position);
+    }
 
     public CategoriesAdapter(String[] captions, int[] imageIds) {
         this.captions = captions;
@@ -27,6 +34,10 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.Vi
         }
     }
 
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int i) {
         CardView cv = (CardView) LayoutInflater.from(parent.getContext())
@@ -35,7 +46,7 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.Vi
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+    public void onBindViewHolder(ViewHolder viewHolder,final int position) {
         CardView cardView = viewHolder.cardView;
         ImageView imageView = (ImageView) cardView.findViewById(R.id.info_image);
         Drawable drawable = cardView.getResources().getDrawable(imageIds[position]);
@@ -43,6 +54,15 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.Vi
         imageView.setContentDescription(captions[position]);
         TextView textView = (TextView) cardView.findViewById(R.id.info_text);
         textView.setText(captions[position]);
+
+        cardView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (listener != null) {
+                    listener.onClick(position);
+                }
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/org/triptrip/CategoryMaterialFragment.java
+++ app/src/main/java/com/org/triptrip/CategoryMaterialFragment.java
@@ -1,5 +1,6 @@
 package com.org.triptrip;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
@@ -15,23 +16,41 @@ public class CategoryMaterialFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        RecyclerView pizzaRecycler = (RecyclerView) inflater.inflate(R.layout.fragment_pizza_material, container, false);
+        RecyclerView categoryRecycler = (RecyclerView) inflater.inflate(R.layout.fragment_pizza_material, container, false);
 
-        String[] pizzaNames = new String[Category.categories.length];
-        for (int i = 0; i < pizzaNames.length; i++) {
-            pizzaNames[i] = Category.categories[i].getName();
+        String[] categoryNames = new String[Category.categories.length];
+        for (int i = 0; i < categoryNames.length; i++) {
+            categoryNames[i] = Category.categories[i].getName();
         }
 
-        int[] pizzaImages = new int[Category.categories.length];
-        for (int i = 0; i < pizzaImages.length; i++) {
-            pizzaImages[i] = Category.categories[i].getImageResourceId();
+        int[] categoryImages = new int[Category.categories.length];
+        for (int i = 0; i < categoryImages.length; i++) {
+            categoryImages[i] = Category.categories[i].getImageResourceId();
         }
 
-        CategoriesAdapter adapter = new CategoriesAdapter(pizzaNames, pizzaImages);
-        pizzaRecycler.setAdapter(adapter);
+        CategoriesAdapter adapter = new CategoriesAdapter(categoryNames, categoryImages);
+        categoryRecycler.setAdapter(adapter);
 
         GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
-        pizzaRecycler.setLayoutManager(layoutManager);
-        return pizzaRecycler;
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        adapter.setListener(new CategoriesAdapter.Listener() {
+            @Override
+            public void onClick(int position) {
+                Intent intent = null;
+                if (position == 0) {
+                    intent = new Intent(getActivity(), EventsActivity.class);
+                } else if (position == 1) {
+                    intent = new Intent(getActivity(), ExperiencesActivity.class);
+                } else if (position == 2) {
+                    intent = new Intent(getActivity(), ServicesActivity.class);
+                } else if (position == 3) {
+                    intent = new Intent(getActivity(), QuestionsAnswersActivity.class);
+                }
+                getActivity().startActivity(intent);
+
+            }
+        });
+        return categoryRecycler;
     }
 }
