9e01529285846f0d5b9f759a80b718ed9021ff2a
Huy Nguyen <nguyenminhhuy08ct@gmail.com>
2018-11-06 13:53:56 +0700

    Add back botton to detail activity

--- app/src/main/java/com/org/triptrip/activity/ServiceDetailActivity.java
+++ app/src/main/java/com/org/triptrip/activity/ServiceDetailActivity.java
@@ -2,6 +2,7 @@ package com.org.triptrip.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.org.triptrip.R;
 
@@ -11,5 +12,11 @@ public class ServiceDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_detail);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
