b70c2834b92f7e4da2013c23025eea2cce7c40bd
Huy Nguyen <nguyenminhhuy08ct@gmail.com>
2018-11-07 17:01:38 +0700

    Fix map view issue

--- app/src/main/java/com/org/triptrip/activity/ServiceDetailActivity.java
+++ app/src/main/java/com/org/triptrip/activity/ServiceDetailActivity.java
@@ -3,6 +3,7 @@ package com.org.triptrip.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,7 +17,7 @@ import com.org.triptrip.R;
 
 public class ServiceDetailActivity extends AppCompatActivity implements OnMapReadyCallback {
     private MapView mapView;
-    private GoogleMap gmap;
+    private GoogleMap mMap;
 
     private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
 
@@ -30,14 +31,19 @@ public class ServiceDetailActivity extends AppCompatActivity implements OnMapRea
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mapView = findViewById(R.id.map_view);
-        mapView.getMapAsync(this);
+        /*mapView = findViewById(R.id.map_view);
+        mapView.getMapAsync(this);*/
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
 
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        gmap = googleMap;
+        /*gmap = googleMap;
         gmap.setMinZoomPreference(12);
 
         gmap.setIndoorEnabled(true);
@@ -54,6 +60,13 @@ public class ServiceDetailActivity extends AppCompatActivity implements OnMapRea
         markerOptions.position(ny);
         gmap.addMarker(markerOptions);
 
-        gmap.moveCamera(CameraUpdateFactory.newLatLng(ny));
+        gmap.moveCamera(CameraUpdateFactory.newLatLng(ny));*/
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
     }
 }
