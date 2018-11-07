ab628bb0200a79a36f72a7d873f0844b57f86e6e
Huy Nguyen <nguyenminhhuy08ct@gmail.com>
2018-11-07 15:55:35 +0700

    Add map view to service detail activity

--- app/src/main/java/com/org/triptrip/activity/ServiceDetailActivity.java
+++ app/src/main/java/com/org/triptrip/activity/ServiceDetailActivity.java
@@ -4,9 +4,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.org.triptrip.R;
 
-public class ServiceDetailActivity extends AppCompatActivity {
+public class ServiceDetailActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private MapView mapView;
+    private GoogleMap gmap;
+
+    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +30,30 @@ public class ServiceDetailActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        mapView = findViewById(R.id.map_view);
+        mapView.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gmap = googleMap;
+        gmap.setMinZoomPreference(12);
+
+        gmap.setIndoorEnabled(true);
+        UiSettings uiSettings = gmap.getUiSettings();
+        uiSettings.setIndoorLevelPickerEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
+        uiSettings.setMapToolbarEnabled(true);
+        uiSettings.setCompassEnabled(true);
+        uiSettings.setZoomControlsEnabled(true);
+
+        LatLng ny = new LatLng(40.7143528, -74.0059731);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(ny);
+        gmap.addMarker(markerOptions);
+
+        gmap.moveCamera(CameraUpdateFactory.newLatLng(ny));
     }
 }
