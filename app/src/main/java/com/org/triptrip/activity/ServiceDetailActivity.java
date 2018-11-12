b8be1f089b55775f4a1fe5fd1fbc8841744ae29e
Huy Nguyen <nguyenminhhuy08ct@gmail.com>
2018-11-12 14:39:52 +0700

    Clean up some code

--- app/src/main/java/com/org/triptrip/activity/ServiceDetailActivity.java
+++ app/src/main/java/com/org/triptrip/activity/ServiceDetailActivity.java
@@ -6,14 +6,12 @@ import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.org.triptrip.R;
@@ -24,7 +22,6 @@ public class ServiceDetailActivity extends AppCompatActivity implements OnMapRea
     private MapView mapView;
     private GoogleMap mMap;
 
-    private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +45,7 @@ public class ServiceDetailActivity extends AppCompatActivity implements OnMapRea
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney, Australia, and move the camera.
+        // Add a marker
         LatLng address = getLocationFromAddress(this, "434 Eglinton Ave W. Toronto, ON");
         mMap.addMarker(new MarkerOptions().position(address).title("Oxygen Yoga and Fitness"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(address, 15f));
@@ -56,6 +53,13 @@ public class ServiceDetailActivity extends AppCompatActivity implements OnMapRea
     }
 
 
+    /**
+     * Get location from address
+     *
+     * @param context
+     * @param strAddress address string
+     * @return
+     */
     public LatLng getLocationFromAddress(Context context, String strAddress)
     {
         Geocoder coder= new Geocoder(context);
