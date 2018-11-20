6d2f2249e7f850881a025487d8655a8b2e284f8b
Huy Nguyen <nguyenminhhuy08ct@gmail.com>
2018-11-20 13:29:50 +0700

    Add search keyword view

--- app/src/main/java/com/org/triptrip/activity/MainActivity.java
+++ app/src/main/java/com/org/triptrip/activity/MainActivity.java
@@ -8,9 +8,11 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import com.org.triptrip.R;
@@ -130,6 +132,27 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem mSearch = menu.findItem(R.id.action_search);
+
+        SearchView mSearchView = (SearchView) mSearch.getActionView();
+        mSearchView.setQueryHint("Search");
+
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                ArrayAdapter mAdapter = new ArrayAdapter(MainActivity.this,
+                        android.R.layout.simple_list_item_1,
+                        getResources().getStringArray(R.array.months_array));
+                mAdapter.getFilter().filter(newText);
+
+                return true;
+            }
+        });
         return true;
     }
 
